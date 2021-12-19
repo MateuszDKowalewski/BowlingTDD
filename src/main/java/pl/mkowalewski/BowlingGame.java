659217e73185b715564ded8916361f2893a786c9
mkowalewski <mateusz.kowalewski@nekken.pl>
2021-12-19 19:01:17 +0100

    Add constructor with frames to BowlingGame.java

--- src/main/java/pl/mkowalewski/BowlingGame.java
+++ src/main/java/pl/mkowalewski/BowlingGame.java
@@ -2,6 +2,12 @@ package pl.mkowalewski;
 
 class BowlingGame {
 
+  Frame[] framesInGame;
+
+  public BowlingGame(Frame[] framesInGame) {
+    this.framesInGame = framesInGame;
+  }
+
   public int calculateScore() {
     return 0;
   }
