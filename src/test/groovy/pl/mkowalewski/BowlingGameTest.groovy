a85f8d9129d1113b0966ac6c09a7f5410e823d09
mkowalewski <mateusz.kowalewski@nekken.pl>
2021-12-19 18:55:34 +0100

    Add not compiling test for calculating score without strikes or spares

--- src/test/groovy/pl/mkowalewski/BowlingGameTest.groovy
+++ src/test/groovy/pl/mkowalewski/BowlingGameTest.groovy
@@ -3,4 +3,24 @@ package pl.mkowalewski
 import spock.lang.Specification
 
 class BowlingGameTest extends Specification {
+
+    def "should calculate score without strikes or spares"() {
+        given:
+        BowlingGame bowlingGame = new BowlingGame(tenFramesWithOneRolledPin())
+
+        when:
+        def score = bowlingGame.calculateScore()
+
+        then:
+        score = 10
+    }
+
+    private Frame[] tenFramesWithOneRolledPin() {
+        Frame[] frames = new Frame[10]
+        for (int i = 0; i < 10; i++) {
+            frames[i] = new Frame(1, 0)
+        }
+        return frames
+    }
+
 }
